0cb933d5b050b3411c0e7732a6a6d3e3cbb7165a
adhanaAshu <ashu.adhana@gmail.com>
2024-08-27 10:32:26 +0000

    the basic app is ready

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module example/ashu/markdown
 
 go 1.22.5
+
+require (
+	github.com/yuin/goldmark v1.7.4
+	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc
+)
+
+require (
+	github.com/alecthomas/chroma/v2 v2.2.0 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
+
+	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
 )
 
 type FileReader struct{}
+
+type Post struct {
+	Content string
+	Author  string
+	Title   string
+}
 
 // read file content {METHOD}
 func (fr FileReader) Read(slug string) (string, error) {
 
 	f, err := os.Open(slug + ".md")
 	// loggging the captured slug
-	log.Println(f)
+	//log.Println(f)
 	if err != nil {
 		return "", err
 	}
@@ -30,7 +40,16 @@ func (fr FileReader) Read(slug string) (string, error) {
 }
 
 // read file content {FUNCTION}
-func PostHandler(fr FileReader) http.HandlerFunc {
+func PostHandler(fr FileReader, tpl *template.Template) http.HandlerFunc {
+
+	// this doesn't get rendered again and again
+	// somehow , i don't understand this as of now.
+	mdRenderer := goldmark.New(goldmark.WithExtensions(
+		highlighting.NewHighlighting(
+			highlighting.WithStyle("dracula"),
+		),
+	))
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		slug := r.PathValue("slug")
 		// loggging the captured slug
@@ -40,7 +59,24 @@ func PostHandler(fr FileReader) http.HandlerFunc {
 			http.Error(w, "Post not found !", http.StatusNotFound)
 			return
 		}
-		fmt.Fprint(w, postMarkdown)
+		var buf bytes.Buffer
+
+		if err := mdRenderer.Convert([]byte(postMarkdown), &buf); err != nil {
+			panic(err)
+		}
+		// print out everything from byte buffer to Writer
+		// fmt.Fprint(w, buf)
+		//io.Copy(w, &buf)
+
+		err = tpl.Execute(w, &Post{
+			Content: buf.String(),
+			Author:  "Ashu Adhana",
+			Title:   " we have only 10 days to live",
+		})
+		if err != nil {
+			http.Error(w, "Error Executing Template", http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
@@ -48,7 +84,9 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /posts/{slug}", PostHandler(FileReader{}))
+	postemplate := template.Must(template.ParseFiles("post.gohtml"))
+
+	mux.HandleFunc("GET /posts/{slug}", PostHandler(FileReader{}, postemplate))
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
